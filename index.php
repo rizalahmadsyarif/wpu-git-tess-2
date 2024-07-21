461a0ab1d902881ee479dd48283bbb2ee4b07a8e
rizalahmadsyarif <rizalahmadsyariff@gmail.com>
2024-07-21 11:08:54 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+
+<body>
+    <h1>hello dunia</h1>
+
+</body>
+
+</html>
